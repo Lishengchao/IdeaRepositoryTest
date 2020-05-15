9a46ec0200835346d498d623e0958212fe40970d
lishengchao <c402187397@163.com>
2020-05-15 15:37:45 +0800

    更新分支1

--- GitTest/src/main/java/com/atguigu/git/TestGit.java
+++ GitTest/src/main/java/com/atguigu/git/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("idea第一次提交");
         System.out.println("创建分支");
+        System.out.println("分支AAAAA");
     }
 }
